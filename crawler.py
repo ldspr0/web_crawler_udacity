a85d9b95f98b14f12efb68d577e726833552c686
Keeper Developer <ldspr0@gmail.com>
2015-04-28 08:14:19 +0300

    add a get_nex_target function

--- crawler.py
+++ crawler.py
@@ -1,3 +1,12 @@
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
 def get_all_links(page):
     links = [];
     while True:
@@ -17,5 +26,5 @@ def crawl_web (seed):
     while toCrawl:
         page = toCrawl.pop();
         if page not in crawled:
-            
+
     return crawled;
