44c5b3309304ab8b4a3e368a48035b937a9f6d24
Alexander Ivanov <ldspr0@gmail.com>
2015-05-07 16:02:54 +0400

    improving the index for not storing same urls

--- crawler.py
+++ crawler.py
@@ -60,7 +60,8 @@ def crawl_web (seed, max_depth):
 def add_to_index (index, keyword, url):
 	for entry in index:
 		if entry[0] == keyword:
-			entry[1].append(url)
+			if url not in entry[1]:
+				entry[1].append(url)
 			return
 	index.append([keyword,[url]])
     
